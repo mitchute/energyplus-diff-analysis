bacc8c76dcdb83bf2241b860fe6b837ec0003bb5
Matt Mitchell <matt.mitchell@nrel.gov>
2021-06-23 11:56:43 -0600

    update readme, other minor cleanups

--- src/plot_vars.py
+++ src/plot_vars.py
@@ -51,23 +51,26 @@ def plot(base_path: str,
 
     # make plots
     for idx, c in enumerate(cols):
-        print(c)
-        base = df_base[c].iloc[min_idx:max_idx].values
-        mod = df_mod[c].iloc[min_idx: max_idx].values
-        diff = base - mod
-        fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-        x = range(min_idx + 1, max_idx + 1)
-        ax1.plot(x, base, label='baseline')
-        ax1.plot(x, mod, label='modified', linestyle='--')
-        ax1.legend()
-        ax1.grid()
-        ax2.plot(x, diff)
-        ax2.set_ylabel("Delta (baseline - modified)")
-        ax2.grid()
-        plt.suptitle("\n".join(wrap(c)))
-        fig_name = c.replace(" ", "_").replace("/", "-")
-        fig_path = plot_dir_path / f"{fig_name}.png"
-        plt.savefig(fig_path, bbox_inches='tight')
+        try:
+            base = df_base[c].iloc[min_idx:max_idx].values
+            mod = df_mod[c].iloc[min_idx: max_idx].values
+            diff = base - mod
+            fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+            x = range(min_idx + 1, max_idx + 1)
+            ax1.plot(x, base, label='baseline')
+            ax1.plot(x, mod, label='modified', linestyle='--')
+            ax1.legend()
+            ax1.grid()
+            ax2.plot(x, diff)
+            ax2.set_ylabel("Delta (baseline - modified)")
+            ax2.grid()
+            plt.suptitle("\n".join(wrap(c)))
+            fig_name = c.replace(" ", "_").replace("/", "-")
+            fig_path = plot_dir_path / f"{fig_name}.png"
+            plt.savefig(fig_path, bbox_inches='tight')
+            print(c)
+        except:
+            pass
 
 
 if __name__ == "__main__":
